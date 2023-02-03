978f0b66210084fcaae7ece005c98b52aab6c623
Antoine DEBOUT <antoine.debout.95@gmail.com>
2023-02-03 18:24:04 +0100

    no message

--- resources/js/Components/About/CardContact.vue
+++ resources/js/Components/About/CardContact.vue
@@ -27,7 +27,8 @@
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               rows="6"
               placeholder="Message"
-          ></textarea>
+          >
+          </textarea>
           <button
               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button">
